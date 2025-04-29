b207616f74ce6bad32c35b8413cb577a53d6355c
OmarAlimammad <157967608+OmarAlimammad@users.noreply.github.com>
2025-04-29 22:39:59 +0400

    Create options.lua

---
+++ lua/options.lua
@@ -0,0 +1,14 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.laststatus = 3
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 200
